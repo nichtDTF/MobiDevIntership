4821ed7698def692b291b43af4eada5a6c11da41
nichtDTF <66942395+nichtDTF@users.noreply.github.com>
2021-06-23 18:13:07 +0300

    Add files via upload

---
+++ MobiDev/Apps/FirstApp/Program.cs
@@ -0,0 +1,143 @@
+﻿/*
+ * Task 1
+ * Order in Online Shop
+ *
+ * by Chuev Oleg
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Order items array
+        /// </summary>
+        protected static List<float> OrderItems = new List<float>();
+
+        /// <summary>
+        /// Discounts in percent
+        /// </summary>
+        protected static List<float> Discounts = new List<float>();
+
+        /// <summary>
+        /// Result sum
+        /// </summary>
+        private static float TotalSum;
+
+        /// <summary>
+        /// Input values
+        /// </summary>
+        private static float Input(string str)
+        {
+            Console.WriteLine("Input values of " + str);
+            float tmp = 0;
+
+            try
+            {
+                tmp = Convert.ToSingle(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine($"User input not a number; To the {str} will be added 0\n");
+            }
+            return tmp;
+        }
+
+        /// <summary>
+        /// Method for calculating
+        /// </summary>
+        /// <returns></returns>
+        private static bool Calculate()
+        {
+            if (OrderItems.Count == Discounts.Count)
+            {
+                int i = 0;
+                foreach (var elem in OrderItems)
+                {
+                    TotalSum += elem - Convert.ToSingle(Discounts[i] * (elem * 0.01));
+                    i++;
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Method for output data
+        /// </summary>
+        private static void Ouput(List<float> or, List<float> ds)
+        {
+            Console.Write("\nOrder: ");
+
+            foreach (var el in or)
+            {
+                Console.Write($"[{el}] ");
+            }
+
+            Console.Write("\nDiscount: ");
+
+            foreach (var elem in ds)
+            {
+                Console.Write($"[{elem}] ");
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Method for clear all data
+        /// </summary>
+        private static void Clear()
+        {
+            OrderItems.Clear();
+            Discounts.Clear();
+            TotalSum = 0;
+        }
+
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args"></param>
+        static void  Main(string[] args)
+        {
+            Console.WriteLine("1 - Add order\n" +
+                              "2 - Add discount\n" +
+                              "3 - Calculate\n" +
+                              "4 - Output\n" +
+                              "5 - Clear\n");
+
+            for (;;)
+            {
+                string key = "";
+                Console.WriteLine("Input key: ");
+                key = Console.ReadLine();
+                switch (key)
+                {
+                    case "1": OrderItems.Add(Input("Order")); break;
+                    case "2": Discounts.Add(Input("Discount")); break;
+                    case "3": 
+                            if(Calculate() == true)
+                                Console.WriteLine($"Total sum: {TotalSum}");
+                            else
+                                Console.WriteLine($"User don`t add enough discounts or orders.\n");
+                            break;
+                    case "4": Ouput(OrderItems, Discounts); break;
+                    case "5": Clear(); break;
+                    default: Console.WriteLine("Wrong key"); break;
+                }
+            }
+
+        }
+    }
+}
---
+++ MobiDev/Apps/SecondApp/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Board
+        /// </summary>
+        static protected char[,] board = new char[2,2];
+
+        /// <summary>
+        /// Calculate function
+        /// </summary>
+        static bool Calculate(char key)
+        {
+            bool flag_Horiz = false;
+            bool flag_Vertical = false;
+            bool flag_Diag = false;
+
+            //Check all Diagonal Combination
+            flag_Diag = ((board[0, 0] == board[1, 1]) && (board[1, 1] == board[2, 2])) ||
+                        ((board[2, 0] == board[1, 1]) && (board[1, 1] == board[0, 2])) && key == board[0,0];
+
+            for (int i = 0; i < 2; i++)
+            {
+                //Check all Horizontal Combination
+                flag_Horiz = (board[i, 0] == board[i, 1]) && (board[i, 1] == board[i, 2]) && key == board[i, 0];
+                //Check all Vertical Combination
+                flag_Vertical = (board[0, i] == board[1, i]) && (board[1, i] == board[2, i]) && key == board[0, i];
+
+                if (flag_Vertical || flag_Horiz || flag_Diag)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Check winner
+        /// </summary>
+        protected static void CheckWinner()
+        {
+            if (!Calculate('O'))
+                if (!Calculate('X'))
+                    Console.WriteLine("''");
+                else
+                    Console.WriteLine('X');
+            else
+                Console.WriteLine('O');
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Output Information
+        /// </summary>
+        static void Output()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{board[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("");
+
+        }
+
+
+        /// <summary>
+        /// main
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            char[,] sit_x_win = {{'X', 'O', 'X'}, {'X', 'O', 'O'}, {'X', 'X', 'O'}};
+            board = sit_x_win;
+            Output();
+            CheckWinner();
+
+            char[,] sit_o_win = { { 'O', 'X', 'O' }, { 'X', 'O', 'O' }, { 'X', 'X', 'O' } };
+            board = sit_o_win;
+            Output();
+            CheckWinner();
+
+            char[,] no_one_win = { { 'X', 'O', 'X' }, { 'O', 'O', 'X' }, { 'X', 'X', 'O' } };
+            board = no_one_win;
+            Output();
+            CheckWinner();
+            
+            Console.ReadLine();
+        }
+
+    }
+}
